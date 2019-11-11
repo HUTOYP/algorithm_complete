143908831f53d38a6191ab7555f71989fce3cd8d
HUTOYP <cnhjq@126.com>
2019-11-11 18:38:42 +0800

    Update code.cpp

--- leetcode.25/code.cpp
+++ leetcode.25/code.cpp
@@ -10,25 +10,18 @@ class Solution {
 public:
     /**
      * 给定头尾指针的链表反转函数
-     * 需要定义二级指针或使用引用，否则ListNode* 仍然是一个值传递
      **/
-    void reverseList(ListNode*& head, ListNode*& tail) {
-        // 头尾节点相同时，直接返回即可
-        if (head == tail) return;
-        
+    ListNode* reverseList(ListNode* head) {
         ListNode* p = head;
         ListNode* q = p->next;
         
-        while (q != tail) {
+        while (q != NULL) {
             p->next = q->next;
             q->next = head;
             head = q;
             q = p->next;
         }
-        
-        q->next = head;
-        head = q;
-        tail = p;
+        return head;
     }
     
     ListNode* reverseKGroup(ListNode* head, int k) {
@@ -51,21 +44,13 @@ public:
             // 记录局部反转链表的后一节点
             ListNode* post = last->next;
             
-            reverseList(first, last);
+            last->next = NULL;
+            pre->next = reverseList(first);
+            first->next = post;
             
-            // 状态推进
-            pre->next = first;
-            last->next = post;
-            pre = last;
+            pre = first;
         }
         
         return virtual_head->next;
     }
 };
-
-
-// 执行结果：
-// 通过
-// 显示详情
-// 执行用时 :24 ms, 在所有 cpp 提交中击败了90.35%的用户
-// 内存消耗 :9.6 MB, 在所有 cpp 提交中击败了97.03%的用户
